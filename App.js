d0d561f036053c3d026e01ea6b2c4367ef3b4c9a
ubulina <ukokko@gmail.com>
2021-10-31 18:21:30 +0200

    calculator & calculator with history

--- App.js
+++ App.js
@@ -1,11 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+
 
 export default function App() {
+
+  const [result, setResult] = useState('') 
+  const [firstNumber, setFirstNumber] = useState('')
+  const [secondNumber, setSecondNumber] = useState('')
+  const [data, setData] = useState([])
+
+ 
+
+  const plusPressed = () => {
+
+    const first = Number(firstNumber)
+    const second = Number(secondNumber)
+
+    const sum = first + second
+    setResult(sum)
+
+    const note = `${first} + ${second} = ${sum}`
+
+    setData([...data, { key: note}])
+
+    setFirstNumber('')
+    setSecondNumber('')
+
+  }
+  
+  const minusPressed = () => {
+
+    const first = Number(firstNumber)
+    const second = Number(secondNumber)
+
+    const subtraction = first - second
+    setResult(subtraction)
+
+    const note = `${first} - ${second} = ${subtraction}`
+
+    setData([...data, { key: note}])
+
+    setFirstNumber('')
+    setSecondNumber('')
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+      <Text>Result: {result}</Text>
+      <TextInput 
+        style={styles.input}
+        onChangeText={firstNumber => setFirstNumber(firstNumber)}
+        value={firstNumber}
+        keyboardType="numeric"
+      />
+      <View style={styles.space} />
+      <TextInput 
+        style={styles.input}
+        onChangeText={secondNumber => setSecondNumber(secondNumber)}
+        value={secondNumber}
+        keyboardType="numeric"
+      />
+    
+    <View style={{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', padding: 20}}>
+      <Button onPress={plusPressed} title=" + " /> 
+      <View style={styles.space} />
+      <Button onPress={minusPressed} title=" - " />
+    </View>  
+      <Text>History</Text>
+      <FlatList
+        data={data}
+        renderItem={({item}) => 
+          <Text>{item.key}</Text>}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +87,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 50
   },
+  space: {
+    width: 10, 
+    height: 10,
+  },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    
+  }
+  
 });
